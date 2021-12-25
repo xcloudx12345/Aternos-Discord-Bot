8e9970eefecf032964fcdcded978d5899b30f395
xcloudx12345 <40192522+xcloudx12345@users.noreply.github.com>
2021-12-25 10:06:37 +0700

    Update AternosBot.py

--- AternosBot.py
+++ AternosBot.py
@@ -1,3 +1,4 @@
+import os
 from aternosapi import AternosAPI
 import discord
 from discord.ext import commands, tasks
@@ -5,7 +6,7 @@ from discord.ext import commands, tasks
 headers_cookie = "ATERNOS_SEC_mqrmraiud8000000=c0pz41e4u6400000; ATERNOS_LANGUAGE=vi; __cf_bm=xIdRLMklWQlLpF0hLii1wb9Nm9vBaN6PEK83yJJjZfs-1640397786-0-ATkrCtee0bwR8N8hWZ7yJ32vV10VcehQmgucUOdDv6jgGi7NFdLT5QmXntYApFnumGiN58tvurbAiDatfBMPIiJ12SxcDkS7RxTU84SjsxmdrSHb8hKutqYUcrFVnquCdQ==; ATERNOS_SESSION=hKXn1P1djKuxSS4KApWCReL3cOE0geM2GaHtmNCJLr28b0fJJ7xoY2v5RXqEPBPxiIqtJUSWVPZmx75AI8HQdtjSVZI9BJIG1L9V; _ga=GA1.2.1312348077.1640397815; _gid=GA1.2.1121790510.1640397815; _pbjs_userid_consent_data=3524755945110770; _lr_retry_request=true; _lr_env_src_ats=false; __gads=ID=8302d1106d2589c3-2295edb284cf00f2:T=1640397818:S=ALNI_MZNgB-u30d8gEqRh9yAky3dgXwKfQ; cto_bundle=A3JD019QM05rYXR4aGhCZW1FU1NOZUVSUDhhdmc4bDh0cnc0OThwZVF6RHlRZHp6MzVmRFFQMnJPeFNTTFFRYmdkV20xTVV4YldtR1l5WVdicGVNeTRpMG9KSVh5RkM0enFBRSUyRnpSanVtWFo4VjRkTFk1M3EwUlhMZEZTazFsWkE2RXc0dWo2SzBnY1RadmdHVEZtZ1FVUVFVZyUzRCUzRA; ATERNOS_SERVER=NpAiwwd2rk4EBV36; cnx_userId=5dd4c1be057244b6b6c783f3b558d6aa; _pubcid=9b529ba3-049d-471f-8035-9b1a96702bb3; _pubcid_sharedid=01FQQNC3M78X9KGRXF8MWECKWB"
 TOKEN = "4WlMhaMqiThQThAj5U42"
 server = AternosAPI(headers_cookie, TOKEN, timeout=10)
-DISCORD = getenv("DISCORD_TOKEN")
+DISCORD = os.getenv("DISCORD_TOKEN")
 
 client = commands.Bot(command_prefix = ["-"])
 client.remove_command('help')
